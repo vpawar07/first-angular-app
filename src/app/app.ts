5083bc2f7a4abad50421847363bdfd916fda0a4b
vpawar07 <vp7600634@gmail.com>
2026-07-29 22:09:34 +0530

    Add header component and user dummy data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
