49e233d0e85ee9734dc6954943acd7a1f8aa9a03
vpawar07 <vp7600634@gmail.com>
2026-07-29 23:42:18 +0530

    Implement user selection functionality with dynamic user data display

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
+    selectedUser = DUMMY_USERS[randomIndex];
+    get imagePath(): string {
+        return `assets/users/${this.selectedUser.avatar}`;
+    }
 
+    onSelectUser() {
+      const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+      this.selectedUser = DUMMY_USERS[randomIndex];  
+    }
 }
