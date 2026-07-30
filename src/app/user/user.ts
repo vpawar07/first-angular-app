7f4fc01f9f4a509e460c18ea64dd95b1e5351472
vpawar07 <vp7600634@gmail.com>
2026-07-30 10:22:45 +0530

    Refactor user component to use signals and computed properties for dynamic user data

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,13 +10,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-    selectedUser = DUMMY_USERS[randomIndex];
-    get imagePath(): string {
-        return `assets/users/${this.selectedUser.avatar}`;
-    }
+    selectedUser = signal(DUMMY_USERS[randomIndex]);
+    imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
+    // get imagePath(): string {
+    //     return `assets/users/${this.selectedUser().avatar}`;
+    // }
 
     onSelectUser() {
       const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-      this.selectedUser = DUMMY_USERS[randomIndex];  
+      this.selectedUser.set(DUMMY_USERS[randomIndex]);
     }
 }
