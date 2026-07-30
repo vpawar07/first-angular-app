7268e535e3ebd655f9968738c9b59d034145b3e5
vpawar07 <vp7600634@gmail.com>
2026-07-30 23:14:42 +0530

    Revert use of output emiter and used the Output decorator

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -10,8 +10,7 @@ export class User {
     @Input({required:true}) id!: string;
     @Input({required:true}) avatar!: string;
     @Input({required:true}) name!: string;
-    // @Output() select = new EventEmitter<string>();
-    select = output<string>();
+    @Output() select = new EventEmitter<string>();
 
     get imagePath(): string {
         return `assets/users/${this.avatar}`;
